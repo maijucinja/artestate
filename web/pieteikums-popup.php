b8f179ec15d9e265e533b606e0745f7c3289082c
maijucinja <maijucinja@gmail.com>
2017-04-07 12:29:27 +0300

    Add files via upload

---
+++ web/pieteikums-popup.php
@@ -0,0 +1,104 @@
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-content">
+    <span class="fa fa-remove close" data-dismiss="modal"></span>
+    <h4 id="ipasums">Piesaki savu īpašumu</h4>
+    <h4>Jūsu kontaktinformācija</h4>
+    <hr>
+
+    <div class="row">
+      <div class="form-group col-sm-6">
+          <label>Jūsu vārds, uzvārds</label>
+          <input type="text" name="" class="form-control">
+        </div>
+        <div class="form-group col-sm-6">
+          <label>E-pasts</label>
+          <input type="text" name="" class="form-control">
+        </div>
+        <div class="col-sm-6 form-group">
+          <label>Tālrunis</label>
+          <input type="text" name="" class="form-control">
+        </div>
+    </div>
+
+  
+
+      <h4>Informācija par īpašumu</h4>
+      <hr>
+        <div class="row form-group">
+
+          <div class="col-sm-6">
+            <label>Pilsēta</label>
+              <select class="form-control">
+                <option>Rīga</option>
+                <option>Ventspils</option>
+              </select>
+          </div>
+          <div class="col-sm-6">
+            <label>Iela</label>
+            <input type="text" name="" class="form-control">
+          </div>
+
+      </div>
+        <div class="form-group">
+        <div class="row">
+          <div class="col-sm-6">
+            <label>Platība m<sup>2</sup></label>
+            <input type="text" name="" class="form-control">
+          </div>
+          <div class="col-sm-6">
+            <label>Istabu skaits</label>
+            <input type="text" name="" class="form-control">
+          </div>
+        </div>
+      </div>
+           <div class="form-group">
+        <div class="row">
+          <div class="col-sm-6">
+            <label>Veids</label>
+              <select class="form-control">
+                <option>..</option>
+                <option>..</option>
+              </select>
+          </div>
+          <div class="col-sm-6">
+            <label>Tips</label>
+              <select class="form-control">
+                <option>Dzīvoklis</option>
+                <option>..</option>
+              </select>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="row">
+          <div class="col-sm-6">
+            <label>Vēlamā cena EUR</label>
+            <input type="text" name="" class="form-control">
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label>Apraksts</label>
+        <textarea class="form-control" rows="3"></textarea>
+      </div>
+      
+
+      <div class="file-upload">
+        <input type="file" name="">
+        <a href="#"><i class="fa fa-picture-o" aria-hidden="true"></i> <span>Pievienot attēlus</span></a>
+      </div>
+      
+        <div class="row">
+          <div class="col-md-3 col-sm-6">
+             <img src="img/katrtelpas1.jpg" class="img-responsive">
+           
+          </div>
+          <div class="col-md-3 col-sm-6">
+             <img src="img/katrtelpas1.jpg" class="img-responsive">
+          </div>
+        </div>
+      <button type="submit" class="btn">Sūtīt</button>
+  </div>
+
+</div>
